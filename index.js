29922b110a0a955e18c63818a73d4678af7e4a1f
Stephen <stephennguyen24@gmail.com>
2024-02-29 22:36:35 +0700

    feat(mainpage) add product card list

--- index.js
+++ index.js
@@ -14,20 +14,65 @@
 const fs = require("fs");
 const http = require("http");
 
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%ID%}/g, product.id);
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  }
+
+  return output;
+};
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf8"
+);
+const temptProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf8"
+);
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf8");
-const dataObject = JSON.parse(data);
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
+
+  // Overview
   if (pathName === "/" || pathName == "/overview") {
-    res.end("This is the OVERVIEW");
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+
+    const cardHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardHtml);
+    res.end(output);
+
+    // Product page
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT");
+
+    // API
   } else if (pathName === "/api") {
     res.writeHead(200, {
       "Content-type": "application/json",
     });
     res.end(data);
+
+    //Not found
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
